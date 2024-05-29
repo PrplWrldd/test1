9380d1c142ee766a72bfcc73d8cb0a8396bc9b2a
PrplWrldd <johanirfan.k@live.iium.edu.my>
2024-05-29 16:03:05 +0800

    change showkitchen

--- resources/views/showkitchen.blade.php
+++ resources/views/showkitchen.blade.php
@@ -9,16 +9,18 @@
 <body class="bg-gray-100 py-8 px-4">
     <table class="w-full text-left bg-white rounded-lg overflow-hidden shadow-lg">
         <thead class="bg-gray-200">
-            <tr>
-                <th class="py-4 px-6">Table Number</th>
-                <th class="py-4 px-6">Food Type</th>
-            </tr>
+        <tr>
+            <th class="py-4 px-6">Table Number</th>
+            <th class="py-4 px-6">Food Type</th>
+            <th class="py-4 px-6">Amount</th> <!-- New column -->
+        </tr>
         </thead>
         <tbody>
             @foreach($data as $item)
             <tr class="{{ $loop->iteration % 2 === 0 ? 'bg-gray-100' : '' }}">
                 <td class="py-4 px-6">{{ $item->table_number }}</td>
-                <td class="py-4 px-6">{{ $item->food_type }}</td>
+                <td class="py-4 px-6">{{ implode(', ', $item->food_type) }}</td> <!-- Display food_type as a comma-separated string -->
+                <td class="py-4 px-6">{{ $item->amount }}</td> <!-- Display amount -->
             </tr>
             @endforeach
         </tbody>
